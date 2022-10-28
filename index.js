f4cd479363c3cad3e883aef9a648821d0262a053
Matt Garnett <matt_garnett@aimltd.uk>
2022-10-28 22:10:01 +0100

    Tidied things up before an intended future refactor

--- index.js
+++ index.js
@@ -1,77 +1,74 @@
-const _typeOf = require ('type-detect');
-global.typeOf = (v)=>{
-		return _typeOf(v).toLowerCase();
+/* eslint-disable no-underscore-dangle */
+const _typeOf = require("type-detect");
+const Lineno = require("lineno");
+const path = require("path");
+const utils = require("dot-wild");
+const chalk = require("chalk");
+
+const polymorphicPatch = require("./lib/polymorphic-patch");
+const patchModelMethods = require("./lib/patch-model-methods");
+
+global.chalk = chalk;
+global.typeOf = (v) => _typeOf(v).toLowerCase();
+global.Lineno = (fn) => {
+    let l = new Lineno(fn);
+    return () => chalk`{gray.bold ${path.basename(l.filename)} @${l.get()}}`;
 };
 
-
-const path = require('path');
-global.Lineno = (fn)=>{
-		let l = new (require('lineno'))(fn);
-		return () =>chalk`{gray.bold ${path.basename(l.filename)} @${l.get()}}`; 
-};
-global.chalk = require('chalk');
-
-
-const polymorphicPatch = require('./lib/polymorphic-patch');
-const patchModelMethods = require('./lib/patch-model-methods');
-
-
-
-module.exports = function definePolymorphicHook (sails) {		
+module.exports = function definePolymorphicHook(sails) {
     // keep the original function
-    const loadModels = sails.hooks.moduleloader.loadModels;
+    const { loadModels } = sails.hooks.moduleloader;
 
-		const utils = require('dot-wild');
-		utils.filter = (data, path) => data.filter(d => utils.has(d, path));
-		
+    utils.filter = (data, route) => data.filter((d) => utils.has(d, route));
+
     return {
-				configure: function(){
-						// console.dir(sails.config);
-				},
-				
-				defaults: {
-						__configKey__: {
-								// tokens used in a populate path
-								tokens: {
-										poly: '*',
-										deep: '.',
-										incl: '&',
-								},
-								polySeam: '-_-', // plymorphic seam used to concatenate the polymorphic attribute key, model name and association attribute
-						}
-				},
-				
-        initialize: function(cb) {
-						
-            // monkey patch the load models function
-            sails.hooks.moduleloader.loadModels = function (cb2) {
+        configure: function () {
+            // console.dir(sails.config);
+        },
+
+        defaults: {
+            __configKey__: {
+                // tokens used in a populate path
+                tokens: {
+                    poly: "*",
+                    deep: ".",
+                    incl: "&",
+                },
+                polySeam: "-_-", // plymorphic seam used to concatenate the polymorphic attribute key, model name and association attribute
+            },
+        },
+
+        initialize: function (cb) {
+            function load(cb2) {
                 // call the original function to do its thing and our patch as the callback function
-                loadModels(function(err, models){
-                    if (err){
+                loadModels((err, models) => {
+                    if (err) {
                         return cb2(err, models);
                     }
 
                     // modify the models according to polymorphic rules
                     // send raw model objects for polymorphic relashionships injection
-										sails.log.info('DEEP-ORM - Patching loadModels with deep orm logic...');
+                    sails.log.info("DEEP-ORM - Patching loadModels with deep orm logic...");
                     models = polymorphicPatch(models);
-                    
+
                     // call the actual cb which is in normal sails pipeline
                     return cb2(err, models);
                 });
-            }   
+            }
 
-            sails.on('hook:orm:loaded', function (){
-                sails.log.info('DEEP-ORM - Patching model methods with deep orm logic...');
+            // monkey patch the load models function
+            sails.hooks.moduleloader.loadModels = load;
+
+            sails.on("hook:orm:loaded", () => {
+                sails.log.info("DEEP-SAILS-ORM - Patching model methods with deep ORM logic...");
 
                 patchModelMethods(sails.models);
-                
+
                 // Then call cb() to finish patching
                 return cb();
             });
-
         },
 
-				utils,
+        utils,
     };
-}
+};
